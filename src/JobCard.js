d2d90ae617772b132437aaf6037ead1168067eef
Ezra Chung <ezra.chung97@gmail.com>
2022-11-30 12:22:51 -0800

    Renamed API to api. Added docstring to JobCard

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,6 +1,15 @@
+import React from "react";
 
+/**
+ * Props: Job object like:
+ *  { id, title, salary, equity, companyHandle, companyName }
+ *
+ * State: None
+ *
+ * RoutesList => JobsList => JobCard
+ */
 
-function JobCard() {
+function JobCard({job}) {
 
 }
 
