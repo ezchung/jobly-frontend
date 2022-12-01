9ccc6e6f8505a848503235d9acb7a8549f3e8813
Jennifer Reuting <jen@reuting.com>
2022-11-30 17:34:40 -0800

    styled job card

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./JobCard.css"
 
 /**
  * Props: Job object like:
@@ -11,13 +13,16 @@ import React from "react";
 
 function JobCard({job}) {
     return (
-        <div className="JobCard">
-        <h3>{job.title}</h3>
-        <p><i>{job.id}</i></p>
-        <p><b>Company:</b> {job.companyName}</p>
-        <p><b>Salary:</b> {job.salary}</p>
-        <p><b>Equity:</b> {job.equity}</p>
-        </div>
+      <div className="JobCard card mb-3">
+        <div className="card-body">
+          <h5 className="card-title">{job.title}</h5>
+    <h6 className="card-subtitle mb-2 text-muted">Job Id: {job.id}</h6>
+    <p className="card-text"><b>Salary:</b> {job.salary}</p>
+    <p className="card-text"><b>Equity:</b> {job.equity}</p>
+    <Link to={`/companies/${job.companyHandle}`}>{job.companyName}</Link>
+  </div>
+</div>
+
   )
 }
 
