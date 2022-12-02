79bc145d5a0d36aebb68795e48d0c1604826a3df
Jennifer Reuting <jen@reuting.com>
2022-12-01 17:14:37 -0800

    Created login form and auth functionality

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { Navigate } from "react-router-dom"
+import userContext from "./userContext";
 
 /**
- * TODO: Edit LoginIn Form variable names
+ * Props: handleLogin from JoblyApp
+ *
+ * State: formData { username, password } from login form
+ *
+ * Returns --> if logged in: companies page
+ * if not logged in: renders login form
+ *
+ * 
  */
 function LoginForm({handleLogin}) {
     const initialFormData = {
@@ -11,7 +20,7 @@ function LoginForm({handleLogin}) {
 
     const [formData, setFormData] = useState(initialFormData);
 
-    console.log("Login Form State -------> ", formData);
+    const { token } = useContext(userContext);
 
     function handleChange(evt){
         const {name, value} = evt.target
@@ -27,30 +36,41 @@ function LoginForm({handleLogin}) {
     }
 
     return (
-        <form className="LoginForm-form" onSubmit={handleSubmit}>
-            <div className="LoginForm-div">
-                <input
-                    id="LoginForm-username"
-                    name="username"
-                    className="form-control"
-                    placeholder="Enter username..."
-                    onChange={handleChange}
-                    value={formData.username}
-                    aria-label="UsernameInput">
-                </input>
-                <input
-                    id="LoginForm-password"
-                    name="password"
-                    className="form-control"
-                    placeholder="Enter password..."
-                    onChange={handleChange}
-                    value={formData.password}
-                    aria-label="PasswordInput"
-                    type="password">
-                </input>
-                <button className="LoginForm-btn">Submit</button>
-            </div>
-        </form>
+        <div>
+            {token
+                ? (
+                    <div>
+                    <Navigate to="/companies"></Navigate>
+                    </div>
+                ) : (
+                    <form className="LoginForm-form" onSubmit={handleSubmit}>
+                    <div className="LoginForm-div">
+                        <input
+                            id="LoginForm-username"
+                            name="username"
+                            className="form-control"
+                            placeholder="Enter username..."
+                            onChange={handleChange}
+                            value={formData.username}
+                            aria-label="UsernameInput">
+                        </input>
+                        <input
+                            id="LoginForm-password"
+                            name="password"
+                            className="form-control"
+                            placeholder="Enter password..."
+                            onChange={handleChange}
+                            value={formData.password}
+                            aria-label="PasswordInput"
+                            type="password">
+                        </input>
+                        <button className="LoginForm-btn">Submit</button>
+                    </div>
+                </form>
+                )
+            }
+        </div>
+
     )
 
 }
